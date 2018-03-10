2fcd8398f442790ce0ec879572fbeecc40583607
Relfos <relfos@gmail.com>
2018-03-10 14:56:26 +0000

    Added website

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nacho-game
+
+Nachomen is a turn based multiplayer game using a Neo smart contract as backend.
+
+In Nachomen players can collect different "luchadores", which can then be pit against other players luchadores.
+
+The game frontend is implemented as a C# game client running on Unity engine.
+
+The smart contract does all the game logic, including game accounts, turn-based battles, and gym (training luchadores) and auction (selling/buying luchadores).
+
+Each time a new Neo address joins the game, the contract generates a new random luchador as a non-fungible token.
---
+++ WrestlingSite/Program.cs
@@ -0,0 +1,34 @@
+﻿using SynkServer.Core;
+using SynkServer.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrestlingSite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize a logger
+            var log = new SynkServer.Core.Logger();
+
+            // either parse the settings from the program args or initialize them manually
+            var settings = ServerSettings.Parse(args);
+
+            var server = new HTTPServer(log, settings);
+
+            // instantiate a new site, the second argument is the file path where the public site contents will be found
+            var site = new Site(server, "public");
+
+            site.Get("/", (request) =>
+            {
+                return HTTPResponse.Redirect("/index.html");
+            });
+
+            server.Run();
+        }
+    }
+}
